b0cf008a8d2ae16e584de4b5d8c2543410e5bd10
Andersen Cypriano <acypriano@mtsolucoes.com.br>
2023-10-30 18:05:18 -0300

    fixed: element trigger footer

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-function insertLogoMTWbuy () {
-  // Infos
-  const urlLogoMT = 'https://mtsolucoes.com.br/wp-content/uploads/2023/08/LOGOTIPO_MT_SOLUCOES.svg';
-  const domainName = window.location.host.split('.')[0];
-  const urlRedirectMT = `https://www.mtsolucoes.com.br/?utm_source=${domainName}&utm_medium=${domainName}`;
-  
-  // Dimensions em px
-  const widthLogo = 150;
-  const paddingLogo = 10;
+<script>
+  function insertLogoMTWbuy () {
+ // Infos
+ const urlLogoMT = 'https://mtsolucoes.com.br/wp-content/uploads/2023/08/LOGOTIPO_MT_SOLUCOES.svg';
+ const domainName = window.location.host.split('.')[1];
+ const urlRedirectMT = `https://www.mtsolucoes.com.br/?utm_source=${domainName}&utm_medium=${domainName}`;
+ 
+ // Dimensions em px
+ const widthLogo = 150;
+ const paddingLogo = 10;
 
-  // Get logo wbuy and clone him to append after this element
-  const logoWbuy = document.querySelector('footer + div a[href="https://www.wbuy.com.br/"]');
-  const clone = logoWbuy .cloneNode(true);
+ // Get logo wbuy and clone him to append after this element
+ const logoWbuy = document.querySelector('footer + div a');
+ const clone = logoWbuy .cloneNode(true);
 
-  // Strings style to element cloned
-  const styleLogoMT = `width: ${widthLogo}px`;
-  const styleImgLogoMT = `width: 100%; height: auto; padding: 0 ${paddingLogo}px;`;
+ // Strings style to element cloned
+ const styleLogoMT = `width: ${widthLogo}px`;
+ const styleImgLogoMT = `width: 100%; height: auto; padding: 0 ${paddingLogo}px;`;
 
-  // Set news attributes and values
-  clone.setAttribute('href', urlRedirectMT);
-  clone.style.cssText = styleLogoMT;
-  clone.children[0].setAttribute('src', urlLogoMT)
-  clone.children[0].setAttribute('alt', 'MT Soluções')
-  clone.children[0].style.cssText = styleImgLogoMT;
+ // Set news attributes and values
+ clone.setAttribute('href', urlRedirectMT);
+ clone.style.cssText = styleLogoMT;
+ clone.children[0].setAttribute('src', urlLogoMT)
+ clone.children[0].setAttribute('alt', 'MT Soluções')
+ clone.children[0].style.cssText = styleImgLogoMT;
 
-  clone.children[0].removeAttribute('data-src');
-  logoWbuy.after(clone)
+ clone.children[0].removeAttribute('data-src');
+ logoWbuy.after(clone)
 }
 
-document.querySelector('footer + div a[href="https://www.wbuy.com.br/"]') ? insertLogoMTWbuy () : null;
+document.querySelector('footer + div a') ? insertLogoMTWbuy () : null;
+</script>
